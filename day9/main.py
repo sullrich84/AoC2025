cdc44e37238dca24dcf8552c2e922df42776a795
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-12-09 21:56:56 +0100

    chore: day9

--- day9/main.py
+++ day9/main.py
@@ -1,9 +1,3 @@
-from icecream import ic
-from collections import defaultdict
-from math import sqrt, prod
-from heapq import heapify, heappop, heappush
-
-
 def parseData(name="task"):
     return [
         (int(x[0]), int(x[1]))
@@ -14,14 +8,9 @@ def parseData(name="task"):
 def solve1(data):
     ans = 0
     for i, p1 in enumerate(data[:-1]):
-        r1, c1 = p1
         for p2 in data[i + 1 :]:
-            r2, c2 = p2
-            h = abs(r2 - r1) + 1
-            w = abs(c2 - c1) + 1
-            area = h * w
-            ans = max([ans, area])
-
+            area = (abs(p2[0] - p1[0]) + 1) * (abs(p2[1] - p1[1]) + 1)
+            ans = max(ans, area)
     return ans
 
 
